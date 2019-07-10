e52b5b119d7ba60559a7b34dacb1c9ceec60d790
nghiadang-vtca-md <52725884+nghiadang-vtca-md@users.noreply.github.com>
2019-07-10 09:51:09 +0700

    Local Bound Demo

---
+++ LocalBound/app/src/main/java/com/example/localbound/LocalBoundActivity.java
@@ -0,0 +1,47 @@
+package com.example.localbound;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LocalBoundActivity extends AppCompatActivity {
+
+    BoundService myService;
+    boolean isBound = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_bound);
+
+        Intent intent = new Intent(this,BoundService.class);
+        bindService(intent, myConnection, Context.BIND_AUTO_CREATE);
+    }
+
+
+    private ServiceConnection myConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            BoundService.MyLocalBinder binder = (BoundService.MyLocalBinder) service;
+            myService = binder.getService();
+            isBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            isBound = false;
+        }
+    };
+
+    public void showTime(View view){
+        String currentTime = myService.getCurrentTime();
+        TextView myTextView = (TextView)findViewById(R.id.myTextView);
+        myTextView.setText(currentTime);
+    }
+}
