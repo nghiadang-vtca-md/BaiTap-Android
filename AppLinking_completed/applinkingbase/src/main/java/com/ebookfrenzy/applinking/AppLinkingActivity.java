3d29969546eeea5bcdbde7d63a0a73fefc8ad18f
nghiadang-vtca-md <52725884+nghiadang-vtca-md@users.noreply.github.com>
2019-07-10 09:16:14 +0700

    App Linking

---
+++ AppLinking_completed/applinkingbase/src/main/java/com/ebookfrenzy/applinking/AppLinkingActivity.java
@@ -0,0 +1,70 @@
+package com.ebookfrenzy.applinking;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class AppLinkingActivity extends AppCompatActivity {
+
+    public static final String LANDMARK_ID = "landmarkID";
+    private static final String TAG = "AppIndexActivity";
+    private EditText idText;
+    private EditText titleText;
+    private EditText descriptionText;
+    private static final int PERSONAL = 1;
+    private static final int PUBLIC = 0;
+    MyDBHandler dbHandler = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_linking);
+
+        idText = (EditText) findViewById(R.id.idText);
+        titleText = (EditText) findViewById(R.id.titleText);
+        descriptionText = (EditText) findViewById(R.id.descriptionText);
+
+        dbHandler = new MyDBHandler(this, null, null, 1);
+    }
+
+    public void findLandmark(View view) {
+
+        if (!idText.getText().equals("")) {
+            Landmark landmark = dbHandler.findLandmark(idText.getText().toString());
+
+            if (landmark != null) {
+
+                Intent intent = new Intent(this, LandmarkActivity.class);
+                String landmarkid = idText.getText().toString();
+                intent.putExtra(LANDMARK_ID, landmarkid);
+                startActivity(intent);
+
+                /*
+                Log.i(TAG, "Found landmark = " + landmark.getTitle());
+                Uri uri = Uri.parse("http://example.com/landmarks/" + landmark.getID());
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                */
+            } else {
+                titleText.setText("No Match");
+            }
+        }
+    }
+
+    public void addLandmark(View view) {
+
+        Landmark landmark =
+                new Landmark(idText.getText().toString(), titleText.getText().toString(),
+                        descriptionText.getText().toString(), 1);
+
+        dbHandler.addLandmark(landmark);
+        idText.setText("");
+        titleText.setText("");
+        descriptionText.setText("");
+
+    }
+}
---
+++ AppLinking_completed/applinkingbase/src/main/java/com/ebookfrenzy/applinking/LandmarkActivity.java
@@ -0,0 +1,108 @@
+package com.ebookfrenzy.applinking;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.net.Uri;
+
+
+public class LandmarkActivity extends AppCompatActivity {
+
+    private static final String TAG = "Database";
+    private TextView titleText;
+    private TextView descriptionText;
+    private Button deleteButton;
+    private Landmark landmark = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landmark);
+
+        titleText = (TextView) findViewById(R.id.titleText);
+        descriptionText = (TextView) findViewById(R.id.descriptionText);
+        deleteButton = (Button) findViewById(R.id.deleteButton);
+
+
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+
+        if (appLinkAction != null) {
+
+            if (appLinkAction.equals("android.intent.action.VIEW")) {
+
+                String landmarkId = appLinkData.getLastPathSegment();
+
+                if (landmarkId != null) {
+                    displayLandmark(landmarkId);
+                }
+            }
+        } else {
+            handleIntent(appLinkIntent);
+        }
+
+
+    }
+
+    private void handleIntent(Intent intent) {
+
+        String landmarkId = intent.getStringExtra(AppLinkingActivity.LANDMARK_ID);
+        displayLandmark(landmarkId);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+
+
+
+    public void deleteLandmark(View view) {
+
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+
+        if (landmark != null) {
+            dbHandler.deleteLandmark(landmark.getID());
+            titleText.setText("");
+            descriptionText.setText("");
+            deleteButton.setEnabled(false);
+        }
+    }
+
+    private void displayLandmark(String landmarkId) {
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+
+        landmark =
+                dbHandler.findLandmark(landmarkId);
+
+        if (landmark != null) {
+
+            if (landmark.getPersonal() == 0) {
+                deleteButton.setEnabled(false);
+            } else {
+                deleteButton.setEnabled(true);
+            }
+
+            titleText.setText(landmark.getTitle());
+            descriptionText.setText(landmark.getDescription());
+        } else {
+            titleText.setText("No Match Found");
+        }
+    }
+
+
+}
