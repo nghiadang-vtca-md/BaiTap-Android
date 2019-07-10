3a4793431b4086f243957ed76689e25ad724cf64
nghiadang-vtca-md <52725884+nghiadang-vtca-md@users.noreply.github.com>
2019-07-10 09:33:48 +0700

    Database Demo

---
+++ Database/app/src/main/java/com/example/database/MyDBHandler.java
@@ -0,0 +1,116 @@
+package com.example.database;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.database.provider.MyContentProvider;
+
+public class MyDBHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "productDB.db";
+    public static final String TABLE_PRODUCTS = "products";
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_PRODUCTNAME = "productName";
+    public static final String COLUMN_QUANTITY = "quantity";
+
+    // add ContentResolver for using ContentProvider
+    private ContentResolver myCR;
+
+    public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+        myCR = context.getContentResolver();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_PRODUCTS_TABLE =
+                "CREATE TABLE " +
+                TABLE_PRODUCTS + "(" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY," +
+                    COLUMN_PRODUCTNAME + " TEXT," +
+                    COLUMN_QUANTITY + " INTEGER" + ")";
+        db.execSQL(CREATE_PRODUCTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS);
+        onCreate(db);
+    }
+
+    public void addProduct(Product product){
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_PRODUCTNAME,product.getProductName());
+        values.put(COLUMN_QUANTITY,product.getQuantity());
+
+//        SQLiteDatabase db = this.getWritableDatabase();
+//        db.insert(TABLE_PRODUCTS,null,values);
+//        db.close();
+
+        myCR.insert(MyContentProvider.CONTENT_URI, values);
+    }
+
+    public Product findProduct(String productname){
+//        String query = "SELECT * FROM " + TABLE_PRODUCTS + " WHERE " +
+//                                COLUMN_PRODUCTNAME + " = \"" + productname + "\"";
+//        SQLiteDatabase db = this.getWritableDatabase();
+//        // Use cursor
+//        Cursor cursor = db.rawQuery(query, null);
+
+        String[] projection = {COLUMN_ID,COLUMN_PRODUCTNAME,COLUMN_QUANTITY};
+        String selection = "productname = \"" + productname + "\"";
+
+        Cursor cursor = myCR.query(MyContentProvider.CONTENT_URI, projection, selection, null,null);
+
+        Product product = new Product();
+
+        if(cursor.moveToFirst()){
+            cursor.moveToFirst();
+            product.setID(Integer.parseInt(cursor.getString(0)));
+            product.setProductName(cursor.getString(1));
+            product.setQuantity(Integer.parseInt(cursor.getString(2)));
+            // finish cursor
+            cursor.close();
+        }
+        else {
+            product = null;
+        }
+        return product;
+    }
+
+    public boolean deleteProduct(String productname){
+        boolean result = false;
+
+//        String query = "SELECT * FROM " +
+//                TABLE_PRODUCTS + " WHERE " +
+//                    COLUMN_PRODUCTNAME + " = \"" + productname + "\"";
+//
+//        SQLiteDatabase db = this.getWritableDatabase();
+//
+//        Cursor cursor = db.rawQuery(query,null);
+//
+//        Product product = new Product();
+//
+//        if(cursor.moveToFirst()){
+//            product.setID(Integer.parseInt((cursor.getString(0))));
+//            db.delete(TABLE_PRODUCTS, COLUMN_ID + " = ?", new String[]{String.valueOf(product.getID())});
+//            // finish cursor
+//            cursor.close();
+//            result = true;
+//        }
+//        db.close();
+
+        String selection = "productname = \"" + productname + "\"";
+        int rowsDeteled = myCR.delete(MyContentProvider.CONTENT_URI, selection, null);
+        if (rowsDeteled > 0)
+            result = true;
+
+        return result;
+    }
+}
