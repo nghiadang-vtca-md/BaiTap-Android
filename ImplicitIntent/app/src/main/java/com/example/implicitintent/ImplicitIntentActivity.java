191c81ec6a65f78eaaa74c53607f45015f12bbc5
nghiadang-vtca-md <52725884+nghiadang-vtca-md@users.noreply.github.com>
2019-07-10 09:47:36 +0700

    Implicit Intent Demo

---
+++ ImplicitIntent/app/src/main/java/com/example/implicitintent/ImplicitIntentActivity.java
@@ -0,0 +1,22 @@
+package com.example.implicitintent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ImplicitIntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_implicit_intent);
+    }
+
+    public void showWebPage(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://baomoi.com/"));
+
+        startActivity(intent);
+    }
+}
