c78399e3ca888687965d9aa3fb35039941cd50fc
nghiadang-vtca-md <52725884+nghiadang-vtca-md@users.noreply.github.com>
2019-07-10 09:48:13 +0700

    Instant App Demo

---
+++ InstantAppDemo/myfeature/src/main/java/com/example/instantappdemo/myfeature/InstantAppActivity.java
@@ -0,0 +1,13 @@
+package com.example.instantappdemo.myfeature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstantAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instant_app);
+    }
+}
