35da087394dae4406f7a778659b05c3760d91d9b
nghiadang-vtca-md <52725884+nghiadang-vtca-md@users.noreply.github.com>
2019-07-10 10:00:35 +0700

    Send Broadcast

---
+++ SendBroadcast/app/src/main/java/com/example/sendbroadcast/SendBroadcastActivity.java
@@ -0,0 +1,42 @@
+package com.example.sendbroadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SendBroadcastActivity extends AppCompatActivity {
+
+    BroadcastReceiver receiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_broadcast);
+
+        configureReceiver();
+    }
+
+    private void configureReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.example.sendbroadcast");
+        filter.addAction("android.intent.action.ACTION_POWER_DISCONNECTED");
+        receiver = new MyReceiver();
+        registerReceiver(receiver, filter);
+    }
+
+    public void broadcastIntent(View view){
+        Intent intent = new Intent();
+        intent.setAction("com.example.sendbroadcast");
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(intent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
+}
